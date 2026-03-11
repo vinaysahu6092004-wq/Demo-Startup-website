8e3ff4d5e95afa5bbdf43a943e26febdfc194ff3
Vinay Sahu <vinaysahu6092004@gmail.com>
2026-03-11 11:07:43 +0530

    fix imports

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import WhoCanApply from "../Components/WhoCanApply"
 import FeaturedStartups from "../Components/FeaturedStartups"
 import CTA from "../Components/CTA"
 import Investors from "../Components/Investor.jsx"
-import HowItWorks from "../Components/HowitWorks"
+import HowItWorks from "../Components/HowitWorks.jsx"
 import BackToTop from "../Components/BackToTop"
 function Home() {
   return (
